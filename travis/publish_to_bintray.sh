6b0cd464dd699ae35caa04f3034dbe5d2e4ef71f
pranavbhole <pranavbhole@gmail.com>
2017-10-31 16:33:38 -0700

    maha-parent

--- travis/publish_to_bintray.sh
+++ travis/publish_to_bintray.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -ev
-
+mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-parent --settings travis/settings-maha-parent.xml
 mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-par-request --settings travis/settings-maha-par-request.xml
 mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-druid-lookups --settings travis/settings-maha-druid-lookups.xml
 mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-core --settings travis/settings-maha-core.xml
