a563f66ecf9d2b42efe7cffa76f27a0f46e11503
Pranav <pranavbhole@gmail.com>
2017-11-02 11:48:14 -0700

    Release prepare and update versions (#17)
    * Release prepare and update versions
    
    * Release prepare and update versions

--- travis/publish_to_bintray.sh
+++ travis/publish_to_bintray.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -ev
+
 mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-parent --settings travis/settings-maha-parent.xml
 mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-par-request --settings travis/settings-maha-par-request.xml
 mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-druid-lookups --settings travis/settings-maha-druid-lookups.xml
@@ -11,3 +12,6 @@ mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.ya
 mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-api-jersey --settings travis/settings-maha-api-jersey.xml
 mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-api-example --settings travis/settings-maha-api-example.xml
 
+mvn release:update-versions -DautoVersionSubmodules=true -Djava.awt.headless=true
+git commit -m "preparing for next release" -a
+git push https://github.com/yahoo/maha.git master
