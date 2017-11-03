85f7798f4f68e81fed1eb5e30b8e4f49fde453ac
Pranav Bhole <pranavbhole@gmail.com>
2017-11-02 23:47:35 -0700

    release setup

--- travis/release_prepare.sh
+++ travis/release_prepare.sh
@@ -4,4 +4,6 @@ set -ev
 
 git checkout master
 git pull https://github.com/yahoo/maha.git master
-mvn -B release:clean release:prepare -Djava.awt.headless=true -Dmaven.test.skip=true -Dscoverage.skip=true -DskipTests -Darguments="-DskipTests -Dscoverage.skip=true"
+mvn versions:set -DremoveSnapshot
+git commit -m "Preparing for release" -a
+#mvn -B release:clean release:prepare -Djava.awt.headless=true -Dmaven.test.skip=true -Dscoverage.skip=true -DskipTests -Darguments="-DskipTests -Dscoverage.skip=true"
