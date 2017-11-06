4893095381301975a0942091515fc8f602a16486
Ryan Wagner <ryanwagner@yahoo-inc.com>
2017-11-06 11:19:46 -0800

    testing Travis change to quite mode

--- travis/release_prepare.sh
+++ travis/release_prepare.sh
@@ -4,6 +4,6 @@ set -ev
 
 git checkout master
 git pull https://github.com/yahoo/maha.git master
-mvn -B versions:set -DremoveSnapshot -Dmaven.test.skip=true -Dscoverage.skip=true -DskipTests -Darguments="-DskipTests -Dscoverage.skip=true"
+mvn -Bq versions:set -DremoveSnapshot -Dmaven.test.skip=true -Dscoverage.skip=true -DskipTests -Darguments="-DskipTests -Dscoverage.skip=true"
 git commit -m "Preparing for release" -a
 #mvn -B release:clean release:prepare -Djava.awt.headless=true -Dmaven.test.skip=true -Dscoverage.skip=true -DskipTests -Darguments="-DskipTests -Dscoverage.skip=true"
