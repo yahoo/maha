25cb078d2c9ec90970aeb68c38601b0220350b2a
Ryan Wagner <ryanwagner@yahoo-inc.com>
2017-12-08 16:48:08 -0800

    scoverage at 85.39%

--- core/src/test/scala/com/yahoo/maha/core/dimension/DimensionTest.scala
+++ core/src/test/scala/com/yahoo/maha/core/dimension/DimensionTest.scala
@@ -33,6 +33,82 @@ class DimensionTest extends FunSuite with Matchers {
     thrown.getMessage should startWith  ("requirement failed: Each dimension must have 1 primary key :")
   }
 
+  test("Copy a Hive dimension without resetAliasIfNotPresent") {
+    val dim : DimensionBuilder = {
+      ColumnContext.withColumnContext { implicit cc =>
+        import HiveExpression._
+        Dimension.newDimension("dim1", HiveEngine, LevelOne, Set(AdvertiserSchema),
+          Set(
+            DimCol("start_time", StrType(), annotations = Set(PrimaryKey))
+            , ConstDimCol("constant", StrType(), "Constant")
+            , DimCol("end_time", StrType(), annotations = Set(EscapingRequired))
+            , HiveDerDimCol("clicks", DecType(), "start_time")
+            , HiveDimCol("new_clicks", DecType())
+            , HivePartDimCol("newest_clicks", DecType())
+
+          )
+          , Option(Map(AsyncRequest -> 400, SyncRequest -> 400))
+        )
+      }
+    }
+
+    val dim2 : DimensionBuilder = {
+      ColumnContext.withColumnContext { implicit cc: ColumnContext =>
+        dim.createSubset("dim1_subset", "dim1", Set("end_time"), Set.empty, Set.empty, None, Map.empty, resetAliasIfNotPresent = false)
+      }
+    }
+  }
+
+  test("Copy a Druid dimension without resetAliasIfNotPresent") {
+    val dim : DimensionBuilder = {
+      ColumnContext.withColumnContext { implicit cc =>
+        import com.yahoo.maha.core.DruidDerivedFunction._
+
+        Dimension.newDimension("dim1", DruidEngine, LevelOne, Set(AdvertiserSchema),
+          Set(
+            DimCol("stats_date", DateType("YYYY-MM-dd"), annotations = Set(PrimaryKey))
+            , DimCol("decodable", IntType(), annotations = Set(EscapingRequired))
+            , DimCol("discard", IntType())
+            , DruidFuncDimCol("clicks", DecType(), DECODE_DIM("{decodable}", "7", "6"))
+            , DruidPostResultFuncDimCol("test_col", IntType(), postResultFunction = START_OF_THE_WEEK("stats_date"))
+          )
+          , Option(Map(AsyncRequest -> 400, SyncRequest -> 400))
+        )
+      }
+    }
+
+    val dim2 : DimensionBuilder = {
+      ColumnContext.withColumnContext { implicit cc: ColumnContext =>
+        dim.createSubset("dim1_subset", "dim1", Set("discard"), Set.empty, Set.empty, None, Map.empty, resetAliasIfNotPresent = false)
+      }
+    }
+  }
+
+  test("Copy an Oracle dimension without resetAliasIfNotPresent") {
+    val dim : DimensionBuilder = {
+      ColumnContext.withColumnContext { implicit cc =>
+        import OracleExpression._
+
+        Dimension.newDimension("dim1", OracleEngine, LevelOne, Set(AdvertiserSchema),
+          Set(
+            DimCol("stats_date", DateType("YYYY-MM-dd"), annotations = Set(PrimaryKey))
+            , DimCol("decodable", IntType(), annotations = Set(EscapingRequired))
+            , DimCol("discard", IntType())
+            , OracleDerDimCol("clicks", StrType(), DECODE_DIM("{decodable}", "7", "6"))
+            , OraclePartDimCol("test_col", IntType())
+          )
+          , Option(Map(AsyncRequest -> 400, SyncRequest -> 400))
+        )
+      }
+    }
+
+    val dim2 : DimensionBuilder = {
+      ColumnContext.withColumnContext { implicit cc: ColumnContext =>
+        dim.createSubset("dim1_subset", "dim1", Set("discard"), Set.empty, Set.empty, None, Map.empty, resetAliasIfNotPresent = false)
+      }
+    }
+  }
+
   test("Copy a Hive dimension with resetAliasIfNotPresent") {
     val dim : DimensionBuilder = {
       ColumnContext.withColumnContext { implicit cc =>
@@ -40,6 +116,7 @@ class DimensionTest extends FunSuite with Matchers {
         Dimension.newDimension("dim1", HiveEngine, LevelOne, Set(AdvertiserSchema),
           Set(
             DimCol("start_time", StrType(), annotations = Set(PrimaryKey))
+            , ConstDimCol("constant", StrType(), "Constant")
             , DimCol("end_time", StrType(), annotations = Set(EscapingRequired))
             , HiveDerDimCol("clicks", DecType(), "start_time")
             , HiveDimCol("new_clicks", DecType())
