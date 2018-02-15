b4796bcccf61ff7dd5d93106b5a603808d2193b0
surabhip <surabhip@yahoo-inc.com>
2018-02-14 16:41:28 -0800

    Retrigger build

--- core/src/test/scala/com/yahoo/maha/core/query/DefaultQueryPipelineFactoryTest.scala
+++ core/src/test/scala/com/yahoo/maha/core/query/DefaultQueryPipelineFactoryTest.scala
@@ -868,6 +868,7 @@ class DefaultQueryPipelineFactoryTest extends FunSuite with Matchers with Before
     assert(queryPipelineTry.isFailure, "should fail if In filter with more than allowed limit")
     assert(queryPipelineTry.failed.get.getMessage === "requirement failed: Failed to find best candidate, forceEngine=Some(Hive), engine disqualifyingSet=Set(Druid), candidates=Set((fact_druid,Druid), (fact_hive,Hive), (fact_oracle,Oracle))")
   }
+  
   test("fail to generate sync query for presto") {
     val request: ReportingRequest = ReportingRequest.forcePresto(getReportingRequestSync(requestWithIdSort))
     val registry = getDefaultRegistry()
