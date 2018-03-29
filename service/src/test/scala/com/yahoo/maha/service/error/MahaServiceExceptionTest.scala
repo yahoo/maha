e7bfd25dac1aac8c93133cdf1d08af5319b56ef2
Ryan Wagner <ryanwagner@yahoo-inc.com>
2018-03-29 10:27:52 -0700

    Fix Exception Test names

--- service/src/test/scala/com/yahoo/maha/service/error/MahaServiceExceptionTest.scala
+++ service/src/test/scala/com/yahoo/maha/service/error/MahaServiceExceptionTest.scala
@@ -3,13 +3,13 @@ package com.yahoo.maha.service.error
 import org.scalatest.{FunSuite, Matchers}
 
 class MahaServiceExceptionTest extends FunSuite with Matchers {
-  test("Test MahaServiceBadRequestException") {
-    val ex : MahaServiceBadRequestException = new MahaServiceBadRequestException(message = "Got a bad request exception!")
+  test("MahaServiceExceptionTest: MahaServiceBadRequestException") {
+    val ex : MahaServiceBadRequestException = MahaServiceBadRequestException(message = "Got a bad request exception!")
     assert(ex.errorCode.equals(400), "BadRequest should throw 400")
   }
 
-  test("Test MahaServiceBadRequestException") {
-    val ex : MahaServiceExecutionException = new MahaServiceExecutionException(message = "Got an execution exception!")
+  test("MahaServiceExceptionTest: MahaServiceExecutionException") {
+    val ex : MahaServiceExecutionException = MahaServiceExecutionException(message = "Got an execution exception!")
     assert(ex.errorCode.equals(500), "ServiceExecution should throw 500")
   }
 
