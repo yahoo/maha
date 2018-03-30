0246f2037c0316a5237a42604e0d724d0eccd0ac
Pranav Bhole <pranavbhole@gmail.com>
2018-03-30 10:24:48 -0700

    stop publishing api-example to bintray

--- travis/publish_to_bintray.sh
+++ travis/publish_to_bintray.sh
@@ -9,10 +9,5 @@ mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.ya
 mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-oracle-executor --settings travis/settings-maha-oracle-executor.xml
 mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-service --settings travis/settings-maha-service.xml
 mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-api-jersey --settings travis/settings-maha-api-jersey.xml
-mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-api-example --settings travis/settings-maha-api-example.xml
 mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-presto-executor --settings travis/settings-maha-presto-executor.xml
 mvn deploy -Dmaven.test.skip=true -DskipTests -Dscoverage.skip --projects com.yahoo.maha:maha-par-request-2 --settings travis/settings-maha-par-request-2.xml
-
-#mvn release:update-versions -DautoVersionSubmodules=true -Djava.awt.headless=true
-#git commit -m "preparing for next release" -a
-#git push https://github.com/yahoo/maha.git master
