07323afa2835c8f820f3132e6564e457c767d1fb
Ryan Wagner <ryanwagner@oath.com>
2018-04-06 15:33:44 -0700

    Fix string coversions and errors

--- service/src/main/scala/com/yahoo/maha/service/curators/DrilldownConfig.scala
+++ service/src/main/scala/com/yahoo/maha/service/curators/DrilldownConfig.scala
@@ -2,42 +2,27 @@ package com.yahoo.maha.service.curators
 
 import com.yahoo.maha.core.request._
 import org.json4s.DefaultFormats
-import org.json4s.JsonAST._
 
-import scala.collection.immutable.Map.Map4
 import scala.util.Try
 
 /**
   * Parse an input JSON and convert it to a DrilldownConfig object.
   */
 trait JsonCurator {
-  implicit val formats = DefaultFormats
+  implicit val formats: DefaultFormats.type = DefaultFormats
   var enforceFilters : Boolean
   var dimension : Field
   var cube : String
   var ordering : List[SortBy]
-  var maxRows : Int
+  var maxRows : BigInt
 }
 
 object DrilldownConfig extends JsonCurator {
   override var enforceFilters: Boolean = false
-  override var dimension: Field = null
+  override var dimension: Field = _
   override var cube: String = ""
   override var ordering: List[SortBy] = List.empty
-  override var maxRows: Int = 1000
-
-  def extractField[T<:JValue](field:T):Any ={
-    field match{
-      case JBool(boolean) => boolean
-      case JString(_) => field.values
-      case JDecimal(_) => field.values
-      case JInt(_) => field.values
-      case JDouble(_) => field.values
-      case JLong(_) => field.values
-      case JNull|JNothing => null
-      case _ => throw new  UnsupportedOperationException("unsupported field type")
-    }
-  }
+  override var maxRows: BigInt = 1000
 
   def validateCuratorConfig(curatorConfigMap: Map[String, CuratorJsonConfig],
                             reportingRequest: ReportingRequest) : Unit = {
@@ -45,13 +30,13 @@ object DrilldownConfig extends JsonCurator {
     val curatorConfig = curatorConfigMap("drillDown")
     val drillDownMap : Map[String, Any] = curatorConfig.json.extract[Map[String, Any]]
 
-    require(drillDownMap.contains("dimension") && Try(drillDownMap("dimension").toString).isSuccess, "CuratorConfig for a DrillDown should have a dimension declared!")
+    require(drillDownMap.contains("dimension"), "CuratorConfig for a DrillDown should have a dimension declared!")
     val drillDim = drillDownMap("dimension").toString
     dimension = Field(drillDim, None, None)
 
-    if(drillDownMap.contains("mr") && Try(drillDownMap("mr").asInstanceOf[Int]).isSuccess) {
-      require(drillDownMap("mr").asInstanceOf[Int] <= 1000, "Max Rows limit of 1000 exceeded.")
-      maxRows = Try(drillDownMap("mr").asInstanceOf[Int]).getOrElse(1000)
+    if(drillDownMap.contains("mr") && Try(drillDownMap("mr").asInstanceOf[BigInt]).isSuccess) {
+      require(drillDownMap("mr").asInstanceOf[BigInt] <= 1000, "Max Rows limit of 1000 exceeded.")
+      maxRows = Try(drillDownMap("mr").asInstanceOf[BigInt]).getOrElse(1000)
     }
 
     if(drillDownMap.contains("enforceFilters") && Try(drillDownMap("enforceFilters").asInstanceOf[Boolean]).isSuccess) enforceFilters = Try(drillDownMap("enforceFilters").asInstanceOf[Boolean]).getOrElse(false)
--- service/src/test/scala/com/yahoo/maha/service/curators/DrilldownConfigTest.scala
+++ service/src/test/scala/com/yahoo/maha/service/curators/DrilldownConfigTest.scala
@@ -1,6 +1,6 @@
 package com.yahoo.maha.service.curators
 
-import com.yahoo.maha.core.request.ReportingRequest
+import com.yahoo.maha.core.request.{ASC, Field, ReportingRequest, SortBy}
 import com.yahoo.maha.service.BaseMahaServiceTest
 import com.yahoo.maha.service.example.ExampleSchema.StudentSchema
 
@@ -44,5 +44,135 @@ class DrilldownConfigTest extends BaseMahaServiceTest {
     drilldownConfig.validateCuratorConfig(reportingRequest.curatorJsonConfigMap, reportingRequest)
 
     println(drilldownConfig)
+    assert(!drilldownConfig.enforceFilters)
+    assert(drilldownConfig.maxRows == 1000)
+    assert(drilldownConfig.ordering.contains(SortBy("Class ID", ASC)))
+    assert(drilldownConfig.dimension == Field("Section ID", None, None))
+    assert(drilldownConfig.cube == "student_performance")
+  }
+
+  test("DrilldownConfig should throw error on max rows.") {
+    val json : String =
+      s"""{
+                          "cube": "student_performance",
+                          "curators" : {
+                            "drillDown" : {
+                              "config" : {
+                                "enforceFilters": "true",
+                                "dimension": "Section ID",
+                                "ordering": [{
+                                              "field": "Class ID",
+                                              "order": "asc"
+                                              }],
+                                "mr": 1001
+                              }
+                            }
+                          },
+                          "selectFields": [
+                            {"field": "Student ID"},
+                            {"field": "Class ID"},
+                            {"field": "Section ID"},
+                            {"field": "Total Marks"}
+                          ],
+                          "sortBy": [
+                            {"field": "Total Marks", "order": "Desc"}
+                          ],
+                          "filterExpressions": [
+                            {"field": "Day", "operator": "between", "from": "2018-01-01", "to": "2018-01-02"},
+                            {"field": "Student ID", "operator": "=", "value": "213"}
+                          ]
+                        }"""
+    val reportingRequestResult = ReportingRequest.deserializeSyncWithFactBias(json.getBytes, schema = StudentSchema)
+    require(reportingRequestResult.isSuccess)
+    val reportingRequest = reportingRequestResult.toOption.get
+
+    val drilldownConfig = DrilldownConfig
+    val thrown = intercept[Exception] {
+      drilldownConfig.validateCuratorConfig(reportingRequest.curatorJsonConfigMap, reportingRequest)
+    }
+    assert(thrown.getMessage.contains("Max Rows limit of 1000 exceeded"))
+  }
+
+  test("DrilldownConfig should throw error on no dimension.") {
+    val json : String =
+      s"""{
+                          "cube": "student_performance",
+                          "curators" : {
+                            "drillDown" : {
+                              "config" : {
+                                "enforceFilters": "true",
+                                "ordering": [{
+                                              "field": "Class ID",
+                                              "order": "asc"
+                                              }],
+                                "mr": 1000
+                              }
+                            }
+                          },
+                          "selectFields": [
+                            {"field": "Student ID"},
+                            {"field": "Class ID"},
+                            {"field": "Section ID"},
+                            {"field": "Total Marks"}
+                          ],
+                          "sortBy": [
+                            {"field": "Total Marks", "order": "Desc"}
+                          ],
+                          "filterExpressions": [
+                            {"field": "Day", "operator": "between", "from": "2018-01-01", "to": "2018-01-02"},
+                            {"field": "Student ID", "operator": "=", "value": "213"}
+                          ]
+                        }"""
+    val reportingRequestResult = ReportingRequest.deserializeSyncWithFactBias(json.getBytes, schema = StudentSchema)
+    require(reportingRequestResult.isSuccess)
+    val reportingRequest = reportingRequestResult.toOption.get
+
+    val drilldownConfig = DrilldownConfig
+    val thrown = intercept[Exception] {
+      drilldownConfig.validateCuratorConfig(reportingRequest.curatorJsonConfigMap, reportingRequest)
+    }
+    assert(thrown.getMessage.contains("CuratorConfig for a DrillDown should have a dimension declared"))
+  }
+
+  test("DrilldownConfig should throw error on wrong DrillDown declaration.") {
+    val json : String =
+      s"""{
+                          "cube": "student_performance",
+                          "curators" : {
+                            "downdrill" : {
+                              "config" : {
+                                "enforceFilters": "true",
+                                "dimension": false,
+                                "ordering": [{
+                                              "field": "Class ID",
+                                              "order": "asc"
+                                              }],
+                                "mr": 1000
+                              }
+                            }
+                          },
+                          "selectFields": [
+                            {"field": "Student ID"},
+                            {"field": "Class ID"},
+                            {"field": "Section ID"},
+                            {"field": "Total Marks"}
+                          ],
+                          "sortBy": [
+                            {"field": "Total Marks", "order": "Desc"}
+                          ],
+                          "filterExpressions": [
+                            {"field": "Day", "operator": "between", "from": "2018-01-01", "to": "2018-01-02"},
+                            {"field": "Student ID", "operator": "=", "value": "213"}
+                          ]
+                        }"""
+    val reportingRequestResult = ReportingRequest.deserializeSyncWithFactBias(json.getBytes, schema = StudentSchema)
+    require(reportingRequestResult.isSuccess)
+    val reportingRequest = reportingRequestResult.toOption.get
+
+    val drilldownConfig = DrilldownConfig
+    val thrown = intercept[Exception] {
+      drilldownConfig.validateCuratorConfig(reportingRequest.curatorJsonConfigMap, reportingRequest)
+    }
+    assert(thrown.getMessage.contains("DrillDown may not be created without a declaration"))
   }
 }
