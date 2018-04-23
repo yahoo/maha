95ee7665a84f4ce49c2620bb163e5f0bbd4c79f9
Ryan Wagner <ryanwagner@oath.com>
2018-04-23 09:59:53 -0700

    Add presto/hive factories

--- service/src/main/scala/com/yahoo/maha/service/factory/QueryExecutoryFactory.scala
+++ service/src/main/scala/com/yahoo/maha/service/factory/QueryExecutoryFactory.scala
@@ -3,6 +3,7 @@
 package com.yahoo.maha.service.factory
 
 import com.yahoo.maha.executor.oracle.OracleQueryExecutor
+import com.yahoo.maha.executor.presto.{PrestoQueryExecutor, PrestoQueryTemplate}
 import com.yahoo.maha.service.MahaServiceConfig
 import com.yahoo.maha.core.query.{ExecutionLifecycleListener, QueryExecutor}
 import com.yahoo.maha.core.request._
@@ -10,7 +11,6 @@ import com.yahoo.maha.executor.druid.{DruidQueryExecutor, DruidQueryExecutorConf
 import com.yahoo.maha.jdbc.JdbcConnection
 import org.json4s.JValue
 
-import _root_.scalaz._
 import scalaz.Validation.FlatMap._
 import scalaz.syntax.applicative._
 
@@ -115,6 +115,69 @@ class DruidQueryExecutoryFactory extends QueryExecutoryFactory {
             closer.register(executor)
             executor
           }
+    }
+  }
+
+  override def supportedProperties: List[(String, Boolean)] = List.empty
+}
+
+class PrestoQueryExecutoryFactory extends QueryExecutoryFactory {
+
+  """
+    |{
+    |"dataSourceFactoryClass": "",
+    |"dataSourceFactoryConfig": [],
+    |"jdbcConnectionFetchSize" 10,
+    |"lifecycleListenerFactoryClass" : "",
+    |"lifecycleListenerFactoryConfig" : [],
+    |"prestoQueryString" : "",
+    |"queryContext" : "",
+    |"queryAttributes" : ""
+    |}
+  """.stripMargin
+
+  override def fromJson(configJson: JValue): MahaServiceConfig.MahaConfigResult[QueryExecutor] =  {
+    import org.json4s.scalaz.JsonScalaz._
+
+    val dataSourceFactoryClassResult: MahaServiceConfig.MahaConfigResult[String] = fieldExtended[String]("dataSourceFactoryClass")(configJson)
+    val dataSourceFactoryConfigResult: MahaServiceConfig.MahaConfigResult[JValue] = fieldExtended[JValue]("dataSourceFactoryConfig")(configJson)
+    val jdbcConnectionFetchSizeOptionResult: MahaServiceConfig.MahaConfigResult[Option[Int]] = fieldExtended[Option[Int]]("jdbcConnectionFetchSize")(configJson)
+    val lifecycleListenerFactoryClassResult: MahaServiceConfig.MahaConfigResult[String] = fieldExtended[String]("lifecycleListenerFactoryClass")(configJson)
+    val lifecycleListenerFactoryConfigResult: MahaServiceConfig.MahaConfigResult[JValue] = fieldExtended[JValue]("lifecycleListenerFactoryConfig")(configJson)
+    val prestoQueryStringResult: MahaServiceConfig.MahaConfigResult[String] = fieldExtended[String]("prestoQueryString")(configJson)
+
+    val jdbcConnetionResult : MahaServiceConfig.MahaConfigResult[JdbcConnection] = for {
+      dataSourceFactoryClass <- dataSourceFactoryClassResult
+      dataSourceFactoryConfig <- dataSourceFactoryConfigResult
+      dataSourceFactory <-  getFactory[DataSourceFactory](dataSourceFactoryClass, this.closer)
+      dataSource <- dataSourceFactory.fromJson(dataSourceFactoryConfig)
+      jdbcConnectionFetchSizeOption <- jdbcConnectionFetchSizeOptionResult
+    } yield {
+      if(jdbcConnectionFetchSizeOption.isDefined) {
+        new JdbcConnection(dataSource, jdbcConnectionFetchSizeOption.get)
+      } else {
+        new JdbcConnection(dataSource)
+      }
+    }
+
+    val lifecycleListener : MahaServiceConfig.MahaConfigResult[ExecutionLifecycleListener] = for {
+      lifecycleListenerFactoryClass <- lifecycleListenerFactoryClassResult
+      lifecycleListenerFactoryConfig <- lifecycleListenerFactoryConfigResult
+      lifecycleListenerFactory <- getFactory[ExecutionLifecycleListenerFactory](lifecycleListenerFactoryClass, this.closer)
+      lifecycleListener <- lifecycleListenerFactory.fromJson(lifecycleListenerFactoryConfig)
+    } yield lifecycleListener
+
+    val prestoQueryTemplate : MahaServiceConfig.MahaConfigResult[PrestoQueryTemplate] = for {
+      prestoQueryString <- prestoQueryStringResult
+    } yield prestoQueryTemplate
+
+    (jdbcConnetionResult |@| prestoQueryTemplate |@| lifecycleListener) {
+      (a, b, c) => {
+
+        val executor = new PrestoQueryExecutor(a, b, c)
+        closer.register(executor)
+        executor
+      }
     }
   }
 
