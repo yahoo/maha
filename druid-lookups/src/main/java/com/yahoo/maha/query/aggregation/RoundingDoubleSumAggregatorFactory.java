2964cbbcbab9d6c4c97ffa44a15e63b56e62bf47
pavanab <pavanab@oath.com>
2018-07-09 12:15:35 -0700

    adding argument check on scale field

--- druid-lookups/src/main/java/com/yahoo/maha/query/aggregation/RoundingDoubleSumAggregatorFactory.java
+++ druid-lookups/src/main/java/com/yahoo/maha/query/aggregation/RoundingDoubleSumAggregatorFactory.java
@@ -3,6 +3,7 @@ package com.yahoo.maha.query.aggregation;
 import com.fasterxml.jackson.annotation.JacksonInject;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Preconditions;
 import io.druid.math.expr.ExprMacroTable;
 import io.druid.query.aggregation.DoubleSumAggregatorFactory;
 
@@ -16,6 +17,8 @@ public class RoundingDoubleSumAggregatorFactory extends DoubleSumAggregatorFacto
     @JsonCreator
     public RoundingDoubleSumAggregatorFactory(@JsonProperty("name") String name, @JsonProperty("fieldName") String fieldName, @JsonProperty("scale") Integer scale, @JsonProperty("expression") String expression, @JacksonInject ExprMacroTable macroTable) {
         super(name, fieldName, expression, macroTable);
+        Preconditions.checkNotNull(scale, "Must have a valid, non-null scale");
+        Preconditions.checkArgument(scale > 0, "Must have a valid, greater than 0 scale");
         this.scale = scale;
     }
 
--- druid-lookups/src/test/java/com/yahoo/maha/query/aggregation/RoundingDoubleSumAggregatorFactoryTest.java
+++ druid-lookups/src/test/java/com/yahoo/maha/query/aggregation/RoundingDoubleSumAggregatorFactoryTest.java
@@ -11,4 +11,14 @@ public class RoundingDoubleSumAggregatorFactoryTest {
         Assert.assertEquals(factory.finalizeComputation(123.45678999d), 123.46d);
         Assert.assertEquals(factory.finalizeComputation(123.45178999d), 123.45d);
     }
+
+    @Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Must have a valid, non-null scale")
+    public void testNullScale() {
+        RoundingDoubleSumAggregatorFactory factory = new RoundingDoubleSumAggregatorFactory("name", "fieldName", null, null, null);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Must have a valid, greater than 0 scale")
+    public void testInvalidScale() {
+        RoundingDoubleSumAggregatorFactory factory = new RoundingDoubleSumAggregatorFactory("name", "fieldName", -1, null, null);
+    }
 }
