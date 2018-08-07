ce97cc4e411c2ea4400271e9846af0ee33514eff
patelh <hiral@hiralpatel.com>
2018-08-07 14:15:33 -0700

    Add file headers for new files

--- request-log/src/main/scala/com/yahoo/maha/log/KryoStringEventBatch.scala
+++ request-log/src/main/scala/com/yahoo/maha/log/KryoStringEventBatch.scala
@@ -1,3 +1,5 @@
+// Copyright 2017, Yahoo Holdings Inc.
+// Licensed under the terms of the Apache License 2.0. Please see LICENSE file in project root for terms.
 package com.yahoo.maha.log
 
 import com.esotericsoftware.kryo.{Kryo, Serializer}
