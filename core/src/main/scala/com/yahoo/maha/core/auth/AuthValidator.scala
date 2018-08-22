05ceaabbff83d556621c3781989f69347403de31
pavanab4u <pavanab4u@yahoo.co.in>
2018-08-22 10:49:32 -0700

    Adding Auth Validation Interface which can be used by any play framework modules (#309)
    * Adding Auth Validation Interface which can be used by any play framework modules
    
    * releasing next revision

---
+++ core/src/main/scala/com/yahoo/maha/core/auth/AuthValidator.scala
@@ -0,0 +1,46 @@
+package com.yahoo.maha.core.auth
+
+import play.api.Configuration
+import play.api.mvc.{RequestHeader, Result, Results}
+
+import scala.collection.immutable.Map
+
+case class ValidationResult(success: Boolean, user: Option[String])
+
+trait AuthValidator {
+  def init(configuration: Configuration)
+  def validate(requestHeader: RequestHeader) : ValidationResult
+  def handleAuthCallback(requestHeader: RequestHeader) : Result
+  def handleAuthFailure(requestHeader: RequestHeader) : Result
+}
+
+class DefaultAuthValidator extends AuthValidator {
+  override def init(configuration: Configuration): Unit = {
+  }
+
+  override def validate(requestHeader: RequestHeader): ValidationResult = {
+    ValidationResult(success = true, user = None)
+  }
+
+  override def handleAuthCallback(requestHeader: RequestHeader): Result = {
+    Results.Ok
+  }
+
+  override def handleAuthFailure(requestHeader: RequestHeader): Result = {
+    Results.Ok
+  }
+}
+
+trait DruidAuthHeaderProvider {
+  def init(configuration: Configuration)
+  def getAuthHeaders : Map[String, String]
+}
+
+class DefaultDruidAuthHeaderProvider extends DruidAuthHeaderProvider {
+  override def init(configuration: Configuration): Unit = {
+  }
+
+  override def getAuthHeaders: Map[String, String] = {
+    Map.empty
+  }
+}
