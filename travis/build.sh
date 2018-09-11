a382aec3f80292291bb5435d7ef32fd9857212c1
pavanab <pavanab@oath.com>
2018-09-11 16:51:36 -0700

    moving build script under travis folder

--- travis/build.sh
+++ travis/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 cd $TRAVIS_BUILD_DIR/druid-manager
-sbt ++$TRAVIS_SCALA_VERSION package
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then sbt ++$TRAVIS_SCALA_VERSION package; else sbt ++$TRAVIS_SCALA_VERSION rpm:packageBin; fi
 cd $TRAVIS_BUILD_DIR
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then bash ./travis/pull_request.sh; else echo "it is master build"; fi
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then bash ./travis/release_prepare.sh; else echo "skipping release"; fi
