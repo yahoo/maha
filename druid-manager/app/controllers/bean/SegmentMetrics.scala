419e0f1b624d4cf5e08378d04ba55b24d0a81034
pavanab4u <pavanab4u@yahoo.co.in>
2018-09-11 11:21:37 -0700

    adding druid-manager (#330)

---
+++ druid-manager/app/controllers/bean/SegmentMetrics.scala
@@ -0,0 +1,10 @@
+// Copyright 2018, Oath Inc.
+// Licensed under the terms of the Apache License 2.0. Please see LICENSE file in project root for terms.
+package controllers.bean
+
+/**
+ * Created by anhngo on 1/12/15.
+ */
+class SegmentMetrics(val sizeI: Long) {
+  val size: Long = sizeI;
+}
