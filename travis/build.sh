051488f8cc39f6628e608308367d41fdc39832b8
pavanab4u <pavanab4u@yahoo.co.in>
2018-09-13 12:02:16 -0700

    Druid manager: build rpm and publish to bintray (#333)
    * adding build script
    
    * moving build script under travis folder
    
    * adding rpm settings
    
    * adding install rpm-build
    
    * adding install rpm-build
    
    * upload rpm
    
    * upload rpm
    
    * publishing druid-manager rpm
    
    * fixing typo

---
+++ travis/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ev
+
+cd $TRAVIS_BUILD_DIR/druid-manager
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ] || [ "${TRAVIS_BRANCH}" != "master" ]; then
+    sbt ++$TRAVIS_SCALA_VERSION package;
+else 
+    VERSION=`git tag| sort -V| tail -1| awk -F"." {'print $1 "." $2 "." $3+1'}`;
+    git tag -a "$VERSION" -m "Tag $VERSION";
+    git push --tags origin;
+    sbt ++$TRAVIS_SCALA_VERSION -Dversion=$VERSION rpm:packageBin;
+    curl -T $TRAVIS_BUILD_DIR/druid-manager/target/rpm/RPMS/noarch/druid-manager-$VERSION-1.noarch.rpm -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/yahoo/rpm/druid-manager/$VERSION/druid-manager-$VERSION-1.noarch.rpm;
+    curl -X POST -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/yahoo/rpm/druid-manager/$VERSION/publish;
+fi
+
+cd $TRAVIS_BUILD_DIR
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then bash ./travis/pull_request.sh; else echo "it is master build"; fi
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then bash ./travis/release_prepare.sh; else echo "skipping release"; fi
+echo "PR= ${TRAVIS_PULL_REQUEST} , and TAG= ${TRAVIS_TAG} , Branch= ${TRAVIS_BRANCH} , Event = ${TRAVIS_EVENT_TYPE}"
+mvn -B -q install
+
