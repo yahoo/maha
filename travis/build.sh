45b0aa90a6772280287fbe6645d3f803842895e2
pavanab <pavanab@oath.com>
2018-09-13 12:31:42 -0700

    hardcoding version

--- travis/build.sh
+++ travis/build.sh
@@ -5,9 +5,13 @@ cd $TRAVIS_BUILD_DIR/druid-manager
 
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ] || [ "${TRAVIS_BRANCH}" != "master" ]; then
     echo "TRAVIS_BUILD_ID= ${TRAVIS_BUILD_ID}";
+    VERSION=$TRAVIS_BUILD_ID;
+    echo "VERSION = ${VERSION}";
     sbt ++$TRAVIS_SCALA_VERSION package;
 else 
-    VERSION="1.1.2";
+    echo "TRAVIS_BUILD_ID= ${TRAVIS_BUILD_ID}";
+    VERSION=$TRAVIS_BUILD_ID;
+    echo "VERSION = ${VERSION}";
     sbt ++$TRAVIS_SCALA_VERSION -Dversion=$VERSION rpm:packageBin;
     curl -T $TRAVIS_BUILD_DIR/druid-manager/target/rpm/RPMS/noarch/druid-manager-$VERSION-1.noarch.rpm -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/yahoo/rpm/druid-manager/$VERSION/druid-manager-$VERSION-1.noarch.rpm;
     curl -X POST -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/yahoo/rpm/druid-manager/$VERSION/publish;
