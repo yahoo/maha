56ed02a31dd44b2c7b1428ae493a7bd0a2e1d833
pavanab <pavanab@oath.com>
2018-09-13 12:18:00 -0700

    checking TRAVIS_BUILD_ID

--- travis/build.sh
+++ travis/build.sh
@@ -7,12 +7,12 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ] || [ "${TRAVIS_BRANCH}" != "master" ]
     echo "TRAVIS_BUILD_ID= ${TRAVIS_BUILD_ID}";
     #sbt ++$TRAVIS_SCALA_VERSION package;
 else 
-    #VERSION=`git tag| sort -V| tail -1| awk -F"." {'print $1 "." $2 "." $3+1'}`;
-    #git tag -a "$VERSION" -m "Tag $VERSION";
-    #git push --tags origin;
-    #sbt ++$TRAVIS_SCALA_VERSION -Dversion=$VERSION rpm:packageBin;
-    #curl -T $TRAVIS_BUILD_DIR/druid-manager/target/rpm/RPMS/noarch/druid-manager-$VERSION-1.noarch.rpm -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/yahoo/rpm/druid-manager/$VERSION/druid-manager-$VERSION-1.noarch.rpm;
-    #curl -X POST -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/yahoo/rpm/druid-manager/$VERSION/publish;
+    VERSION=`git tag| sort -V| tail -1| awk -F"." {'print $1 "." $2 "." $3+1'}`;
+    git tag -a "$VERSION" -m "Tag $VERSION";
+    git push --tags origin;
+    sbt ++$TRAVIS_SCALA_VERSION -Dversion=$VERSION rpm:packageBin;
+    curl -T $TRAVIS_BUILD_DIR/druid-manager/target/rpm/RPMS/noarch/druid-manager-$VERSION-1.noarch.rpm -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/yahoo/rpm/druid-manager/$VERSION/druid-manager-$VERSION-1.noarch.rpm;
+    curl -X POST -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/yahoo/rpm/druid-manager/$VERSION/publish;
 fi
 
 #cd $TRAVIS_BUILD_DIR
