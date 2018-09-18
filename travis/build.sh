f53174b5a2b4cb7839c56973e1840b4476fbc42a
Pranav <pranavbhole@gmail.com>
2018-09-18 16:29:52 -0700

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -8,7 +8,7 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ] || [ "${TRAVIS_BRANCH}" != "master" ]
 else 
     VERSION="1.1.3";
     echo "VERSION = ${VERSION}";
-    sbt ++$TRAVIS_SCALA_VERSION -Dversion=$VERSION rpm:packageBin > sbt_package.out;
+    sbt ++$TRAVIS_SCALA_VERSION -Dversion=$VERSION rpm:packageBin;
     curl -T $TRAVIS_BUILD_DIR/druid-manager/target/rpm/RPMS/noarch/druid-manager-$VERSION-1.noarch.rpm -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/yahoo/rpm/druid-manager/$VERSION/druid-manager-$VERSION-1.noarch.rpm;
     curl -X POST -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/yahoo/rpm/druid-manager/$VERSION/publish;
 fi
