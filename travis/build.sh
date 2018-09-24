ae306e7112e76bcb54484708f873a0450625b017
Pranav Bhole <pranavbhole@gmail.com>
2018-09-24 15:54:57 -0700

    skip DEBUG and INFO from build output

--- travis/build.sh
+++ travis/build.sh
@@ -18,5 +18,5 @@ cd $TRAVIS_BUILD_DIR
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then bash ./travis/pull_request.sh; else echo "it is master build"; fi
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then bash ./travis/release_prepare.sh; else echo "skipping release"; fi
 echo "PR= ${TRAVIS_PULL_REQUEST} , and TAG= ${TRAVIS_TAG} , Branch= ${TRAVIS_BRANCH} , Event = ${TRAVIS_EVENT_TYPE}"
-mvn -B -q install
+mvn -B -q install | grep -v DEBUG | grep -v INFO
 
