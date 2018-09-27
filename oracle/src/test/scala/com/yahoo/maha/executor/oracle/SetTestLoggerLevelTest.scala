e7c541f045fdfdb4d7a7844de357e7066851ec02
Pranav Bhole <pranavbhole@gmail.com>
2018-09-27 16:27:51 -0700

    Set log level in test

---
+++ oracle/src/test/scala/com/yahoo/maha/executor/oracle/SetTestLoggerLevelTest.scala
@@ -0,0 +1,16 @@
+package com.yahoo.maha.executor.oracle
+
+import org.scalatest.FunSuite
+
+/*
+    Created by pranavbhole on 9/27/18
+*/
+class SetTestLoggerLevelTest extends FunSuite {
+
+  test("set test logger level to error") {
+    org.slf4j.LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
+      .asInstanceOf[ch.qos.logback.classic.Logger]
+      .setLevel(ch.qos.logback.classic.Level.ERROR)
+  }
+
+}
