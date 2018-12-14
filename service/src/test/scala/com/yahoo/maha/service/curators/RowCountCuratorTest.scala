87d20a1e781cb6e346aed0f9edabdabbf0f766a5
Ryan Wagner <ryanwagner@oath.com>
2018-12-14 14:15:30 -0800

    Add test to illustrate removal of non-PK columns from TotalRow request

--- service/src/test/scala/com/yahoo/maha/service/curators/RowCountCuratorTest.scala
+++ service/src/test/scala/com/yahoo/maha/service/curators/RowCountCuratorTest.scala
@@ -4,6 +4,7 @@ package com.yahoo.maha.service.curators
 
 import com.yahoo.maha.core.RequestModel
 import com.yahoo.maha.core.bucketing.{BucketParams, UserInfo}
+import com.yahoo.maha.core.query.OracleQuery
 import com.yahoo.maha.core.request.{DebugValue, Parameter, ReportingRequest}
 import com.yahoo.maha.jdbc._
 import com.yahoo.maha.parrequest2.future.ParRequest
@@ -12,12 +13,13 @@ import com.yahoo.maha.service.utils.{CuratorMahaRequestLogHelper, MahaRequestLog
 import com.yahoo.maha.service.{BaseMahaServiceTest, CuratorInjector, MahaRequestContext}
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
-import org.scalatest.BeforeAndAfterAll
+import org.scalatest.{BeforeAndAfterAll, Matchers}
+import com.yahoo.maha.core._
 
 /**
  * Created by pranavbhole on 24/04/18.
  */
-class RowCountCuratorTest  extends BaseMahaServiceTest with BeforeAndAfterAll {
+class RowCountCuratorTest  extends BaseMahaServiceTest with BeforeAndAfterAll with Matchers {
   val today: String = DateTimeFormat.forPattern("yyyy-MM-dd").print(DateTime.now())
   val yesterday: String = DateTimeFormat.forPattern("yyyy-MM-dd").print(DateTime.now().minusDays(1))
 
@@ -268,6 +270,84 @@ class RowCountCuratorTest  extends BaseMahaServiceTest with BeforeAndAfterAll {
 
     assert(curatorInjector.curatorList.nonEmpty)
     assert(curatorInjector.orderedResultList.nonEmpty)
+  }
+
+  test("Test totalrows with non-PK dim filter") {
+
+    val jsonRequest = s"""{
+                          "cube": "student_performance",
+                          "curators" : {
+                            "totalmetrics" : {
+                              "config" : {
+                              }
+                            }
+                          },
+                          "selectFields": [
+                            {"field": "Student ID"},
+                            {"field": "Section ID"},
+                            {"field": "Total Marks"},
+                            {"field": "Remarks"}
+                          ],
+                          "filterExpressions": [
+                            {"field": "Day", "operator": "between", "from": "$fromDate", "to": "$toDate"},
+                            {"field": "Student ID", "operator": "=", "value": "213"},
+                            {"field": "Remarks", "operator": "=", "value": "Acceptable."}
+                          ],
+                          "includeRowCount" : true,
+                          "forceDimensionDriven" : true
+                        }"""
+    val reportingRequestResult = ReportingRequest.deserializeSyncWithFactBias(jsonRequest.getBytes, schema = StudentSchema)
+    require(reportingRequestResult.isSuccess)
+    val reportingRequest = reportingRequestResult.toOption.get.copy(additionalParameters = Map(Parameter.Debug -> DebugValue(value = true)))
+
+    val bucketParams = BucketParams(UserInfo("uid", true))
+
+    val mahaRequestContext = MahaRequestContext(REGISTRY,
+      bucketParams,
+      reportingRequest,
+      jsonRequest.getBytes,
+      Map.empty, "rid", "uid")
+
+    val mahaRequestLogHelper = MahaRequestLogHelper(mahaRequestContext, mahaServiceConfig.mahaRequestLogWriter)
+    val curatorMahaRequestLogHelper =  CuratorMahaRequestLogHelper(mahaRequestLogHelper)
+
+    val rowCountCurator = RowCountCurator()
+    val curatorInjector = new CuratorInjector(2, mahaService, mahaRequestLogHelper, Set.empty)
+
+    val rowCountCuratorResult: Either[CuratorError, ParRequest[CuratorResult]] = rowCountCurator
+      .process(Map.empty, mahaRequestContext, mahaService, curatorMahaRequestLogHelper, NoConfig, curatorInjector)
+
+    assert(rowCountCuratorResult.isRight)
+    val parReq = rowCountCuratorResult.right.get
+
+    val result = parReq.get(1000)
+    val parReqOption = result.right.get.parRequestResultOption
+    assert(parReqOption.isDefined)
+
+    assert(parReqOption.get.queryPipeline.isSuccess)
+    val totalRowQueryPipelineTry = parReqOption.get.queryPipeline
+    assert(totalRowQueryPipelineTry.isSuccess)
+    val totalRowQueryPipeline = totalRowQueryPipelineTry.get
+
+    assert(totalRowQueryPipeline.factBestCandidate.isEmpty)
+    assert(totalRowQueryPipeline.bestDimCandidates.nonEmpty)
+
+    val totalRowsQuery = totalRowQueryPipeline.queryChain.drivingQuery.asInstanceOf[OracleQuery].asString
+    val expectedTotalRowQuery_correct =
+      s"""
+         |SELECT /*+ FIRST_ROWS */ *
+         |      FROM (SELECT s0.id "Student ID", Count(*) OVER() TOTALROWS, ROWNUM as ROW_NUMBER
+         |            FROM
+         |                (SELECT  id
+         |            FROM student
+         |            WHERE (id = 213)
+         |             ) s0
+         |
+         |
+ |           )
+         |             WHERE ROW_NUMBER >= 1 AND ROW_NUMBER <= 1
+       """.stripMargin
+    totalRowsQuery should equal(expectedTotalRowQuery_correct)(after being whiteSpaceNormalised)
   }
 
   test("Test curator Injection Failure") {
