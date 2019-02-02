0555e8eb4297479d489216719acfcec25ccf1db6
Kevin Chen <kevinchen@oath.com>
2019-02-01 17:38:13 -0800

    append unit test for lookup override

--- core/src/main/scala/com/yahoo/maha/core/query/druid/DruidQueryGenerator.scala
+++ core/src/main/scala/com/yahoo/maha/core/query/druid/DruidQueryGenerator.scala
@@ -502,7 +502,7 @@ class DruidQueryGenerator(queryOptimizer: DruidQueryOptimizer
                 case DruidFuncDimCol(_, _, _, df, _, _, _) =>
                   df match {
                     case LOOKUP(_, _, _) => true
-                    case LOOKUP_WITH_EMPTY_VALUE_OVERRIDE(_, _ ,_, _) => true
+                    case LOOKUP_WITH_EMPTY_VALUE_OVERRIDE(_, _, _, _) => true
                     case LOOKUP_WITH_DECODE(_, _, _, args @ _*) => true
                     case LOOKUP_WITH_DECODE_RETAIN_MISSING_VALUE(_, _, _, _, _, args @ _*) => true
                     case LOOKUP_WITH_DECODE_ON_OTHER_COLUMN( _, _, _, _, _, _) => true
