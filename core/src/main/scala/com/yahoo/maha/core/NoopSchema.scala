333435e4c8e23625e4850ac8e20bb31ec1675efa
Ritvik Jaiswal <ritvikjaiswal@users.noreply.github.com>
2019-04-04 15:58:22 -0700

    Comments

--- core/src/main/scala/com/yahoo/maha/core/NoopSchema.scala
+++ core/src/main/scala/com/yahoo/maha/core/NoopSchema.scala
@@ -1,12 +1,18 @@
-// Copyright 2017, Yahoo Holdings Inc.
+// Copyright 2019, Verizon Media.
 // Licensed under the terms of the Apache License 2.0. Please see LICENSE file in project root for terms.
 package com.yahoo.maha.core
+
+/**
+  * Created by ritvikj on 4/4/19.
+  */
 
 import enumeratum.{Enum, EnumEntry}
 import enumeratum.EnumEntry.{Snakecase, Uppercase}
 
 /*
-  NoopSchema should be used to restrict user queries from hitting intermediate rollups.
+  NoopSchema should be used to prevent user queries from hitting intermediate rollups that are not valid tables. Such
+  rollups are only used as an intermediate step for different child rollups and do not represent actual tables in the
+  database. Restricting these rollups to NoopSchema ensures that no user query actually hits these non-existent tables.
  */
 sealed abstract class NoopSchema (override val entryName: String) extends EnumEntry with Snakecase with Uppercase with Schema
 
