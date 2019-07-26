0d87de2da4dada965927a9784dfc1e1e0302346f
Ryan Wagner <ryanwagner@oath.com>
2019-07-26 11:24:46 -0700

    Move declaration

--- druid-lookups/src/main/java/com/yahoo/maha/maha_druid_lookups/query/lookup/namespace/ExtractionNamespace.java
+++ druid-lookups/src/main/java/com/yahoo/maha/maha_druid_lookups/query/lookup/namespace/ExtractionNamespace.java
@@ -10,8 +10,8 @@ import com.google.common.collect.ImmutableList;
 @JsonSubTypes(value = {
         @JsonSubTypes.Type(name = "mahajdbcleaderfollower", value = JDBCExtractionNamespaceWithLeaderAndFollower.class),
         @JsonSubTypes.Type(name = "mahajdbc", value = JDBCExtractionNamespace.class),
-        @JsonSubTypes.Type(name = "mahainmemorydb", value = RocksDBExtractionNamespace.class),
         @JsonSubTypes.Type(name = "maharocksdb", value = RocksDBExtractionNamespace.class),
+        @JsonSubTypes.Type(name = "mahainmemorydb", value = RocksDBExtractionNamespace.class),
         @JsonSubTypes.Type(name = "mahamongo", value = MongoExtractionNamespace.class)
 })
 public interface ExtractionNamespace {
