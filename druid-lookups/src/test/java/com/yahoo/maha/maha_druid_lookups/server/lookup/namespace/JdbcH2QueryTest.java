d2ea04ff2f2e66cfc160733c264c11cd33e23226
Hao Wang <hwang2@verizonmedia.com>
2020-03-22 15:11:34 -0700

    fix unit test

--- druid-lookups/src/test/java/com/yahoo/maha/maha_druid_lookups/server/lookup/namespace/JdbcH2QueryTest.java
+++ druid-lookups/src/test/java/com/yahoo/maha/maha_druid_lookups/server/lookup/namespace/JdbcH2QueryTest.java
@@ -255,8 +255,7 @@ public class JdbcH2QueryTest {
         doCallRealMethod().when(myJdbcEncFactory).populateLastUpdatedTime(any(), any());
         doCallRealMethod().when(myJdbcEncFactory).getBaseWhereClause(any(), any());
         doCallRealMethod().when(myJdbcEncFactory).populateRowListFromJDBC(any(), any(), any(), any(), any(), any());
-        doCallRealMethod().when(myJdbcEncFactory).getSecondaryTsWhereClause(any(), any());
-        doCallRealMethod().when(myJdbcEncFactory).getMaxValFromColumn(any(), any(), any(), any());
+        doCallRealMethod().when(myJdbcEncFactory).getSecondaryTsWhereClause(any(), any(), any());
         doCallRealMethod().when(myJdbcEncFactory).getMaxValFromColumn(any(), any(), any(), any(), any());
 
         Whitebox.setInternalState(myJdbcEncFactory, "dbiCache", new ConcurrentHashMap<>());
