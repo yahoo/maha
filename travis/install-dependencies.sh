1f2886f2af90ddecf271d74e05fb410ec668bed3
Pranav Anil Bhole <pranavbhole@gmail.com>
2021-04-20 15:39:53 -0700

    fix ubuntu key on build

--- travis/install-dependencies.sh
+++ travis/install-dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ev
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6B05F25D762E3157
 sudo apt update
 sudo apt-get update
 sudo apt-get install rpm
