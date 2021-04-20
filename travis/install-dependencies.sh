e3def8ced5662b9395fadee5dd748c43906287ae
Pranav Anil Bhole <pranavbhole@gmail.com>
2021-04-20 16:26:07 -0700

    apt update with retry

--- travis/install-dependencies.sh
+++ travis/install-dependencies.sh
@@ -2,6 +2,5 @@
 set -ev
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6B05F25D762E3157
-sudo apt update
-sudo apt-get update
+sudo apt-get update --option Acquire::Retries=100 --option Acquire::http::Timeout="60"
 sudo apt-get install rpm
