fca52167fdf3cfe851e9343da3c71ddd22d3bd8c
Pranav Anil Bhole <pranavbhole@gmail.com>
2021-06-20 11:45:03 -0700

    update key enc

--- screwdriver/publish_to_maven.sh
+++ screwdriver/publish_to_maven.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 
 set -ev
-#openssl aes-256-cbc -K $encrypted_f094dd62560a_key -iv $encrypted_f094dd62560a_iv -in travis/pubring.gpg.enc -out travis/pubring.gpg -d
-#gpg --fast-import travis/pubring.gpg
 
 export GPG_TTY=$(tty)
 
 mkdir -p screwdriver/deploy
 chmod 0700 screwdriver/deploy
 
-#openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in screwdriver/pubring.gpg.enc -out screwdriver/deploy/pubring.gpg -d
-#openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in screwdriver/secring.gpg.enc -out screwdriver/deploy/secring.gpg -d
-#export PASS_PHRASE=test
+openssl aes-256-cbc -pass pass:$PASS_PHRASE -in screwdriver/sec.key.enc -out screwdriver/deploy/sec.key -d
+gpg --batch --import screwdriver/deploy/sec.key
 
-#mvn -B deploy -P ossrh -Dmaven.test.skip=true --settings screwdriver/settings/settings-publish.xml
 ls -l screwdriver/deploy
 
 mvn deploy -Dgpg.passphrase=${PASS_PHRASE} -Dgpg.skip=false -Dmaven.test.skip=true -Dscoverage.skip=true -DskipTests -Darguments="-DskipTests -Dscoverage.skip=true" --projects com.yahoo.maha:maha-parent --settings screwdriver/maven-settings.xml
@@ -31,6 +27,5 @@ mvn deploy -Dgpg.passphrase=${PASS_PHRASE} -Dgpg.skip=false -Dmaven.test.skip=tr
 mvn deploy -Dgpg.passphrase=${PASS_PHRASE} -Dgpg.skip=false -Dmaven.test.skip=true -Dscoverage.skip=true -DskipTests -Darguments="-DskipTests -Dscoverage.skip=true" --projects com.yahoo.maha:maha-service --settings screwdriver/maven-settings.xml
 mvn deploy -Dgpg.passphrase=${PASS_PHRASE} -Dgpg.skip=false -Dmaven.test.skip=true -Dscoverage.skip=true -DskipTests -Darguments="-DskipTests -Dscoverage.skip=true" --projects com.yahoo.maha:maha-worker --settings screwdriver/maven-settings.xml
 mvn deploy -Dgpg.passphrase=${PASS_PHRASE} -Dgpg.skip=false -Dmaven.test.skip=true -Dscoverage.skip=true -DskipTests -Darguments="-DskipTests -Dscoverage.skip=true" --projects com.yahoo.maha:maha-api-jersey --settings screwdriver/maven-settings.xml
-
 
 rm -rf screwdriver/deploy
