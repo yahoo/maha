658b982fe84842cf0458b7c41b9d75f3fb02e074
Pranav Anil Bhole <pranavbhole@gmail.com>
2021-06-20 19:19:37 -0700

    cleanup and nexus autoclose

--- screwdriver/release_prepare.sh
+++ screwdriver/release_prepare.sh
@@ -3,8 +3,7 @@
 set -ev
 
 mvn -q -B versions:set -DremoveSnapshot -Dmaven.test.skip=true -Dscoverage.skip=true -DskipTests -Darguments="-DskipTests -Dscoverage.skip=true"
-git config --global user.name "Maha"
-git config --global user.email maha@maha-example.com
+git config --global user.name "Maha Headless"
+git config --global user.email maha@yahoo-inc.com
 # stays only in local
 git commit -m "Preparing for release" -a
-#mvn -B release:clean release:prepare -Djava.awt.headless=true -Dmaven.test.skip=true -Dscoverage.skip=true -DskipTests -Darguments="-DskipTests -Dscoverage.skip=true"
