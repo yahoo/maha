421be8aea7a7e50a4a796c3ede83be00bb83e070
myang01 <mengchieh.yang@oath.com>
2021-07-02 09:39:31 -0700

    logger change

--- druid-lookups/src/main/java/com/yahoo/maha/maha_druid_lookups/query/lookup/dynamic/schema/DynamicLookupCoreSchemaFactory.java
+++ druid-lookups/src/main/java/com/yahoo/maha/maha_druid_lookups/query/lookup/dynamic/schema/DynamicLookupCoreSchemaFactory.java
@@ -3,13 +3,13 @@ package com.yahoo.maha.maha_druid_lookups.query.lookup.dynamic.schema;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.protobuf.Descriptors;
 import com.yahoo.maha.maha_druid_lookups.query.lookup.namespace.*;
-import org.apache.druid.java.util.common.logger.Logger;
+import org.slf4j.*;
 
 import java.io.IOException;
 
 public class DynamicLookupCoreSchemaFactory {
 
-    private static final Logger LOG = new Logger(DynamicLookupCoreSchemaFactory.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DynamicLookupCoreSchemaFactory.class);
 
     public static DynamicLookupCoreSchema buildSchema(DynamicLookupSchema dynamicLookupSchema) throws Descriptors.DescriptorValidationException {
 
