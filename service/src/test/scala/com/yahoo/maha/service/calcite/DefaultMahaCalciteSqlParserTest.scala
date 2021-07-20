4b074335c6427ed435acf1b88bb256c026bffd46
Ritvik Jaiswal <ritvikjaiswal@users.noreply.github.com>
2021-07-20 16:40:20 -0700

    Maha calcite NotEqualToFilter

--- service/src/test/scala/com/yahoo/maha/service/calcite/DefaultMahaCalciteSqlParserTest.scala
+++ service/src/test/scala/com/yahoo/maha/service/calcite/DefaultMahaCalciteSqlParserTest.scala
@@ -336,4 +336,23 @@ class DefaultMahaCalciteSqlParserTest extends BaseMahaServiceTest with Matchers 
 
     assert(request.filterExpressions.toString contains "EqualityFilter(Student ID,123,false,false)")
   }
+
+  test("test filter: not equals") {
+
+    val sql =
+      s"""
+              select * from student_performance
+              where 'Student ID' != 123
+              """
+
+    val mahaSqlNode: MahaSqlNode = defaultMahaCalciteSqlParser.parse(sql, StudentSchema, "er")
+    assert(mahaSqlNode.isInstanceOf[SelectSqlNode])
+    val request = mahaSqlNode.asInstanceOf[SelectSqlNode].reportingRequest
+    assert(request.requestType === SyncRequest)
+    assert(request.filterExpressions.size == 1)
+    assert(request.filterExpressions.head.operator.toString.equals("<>"))
+    assert(request.filterExpressions.head.field.equals("Student ID"))
+    assert(request.filterExpressions.head.asValues.equals("123"))
+    assert(request.filterExpressions.toString contains "NotEqualToFilter(Student ID,123,false,false)")
+  }
 }
