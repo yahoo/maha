04e7a082e15c1b26cdefa73d96c8f06f18d92b15
nbabu01 <naishu.babu@verizonmedia.com>
2021-07-20 15:28:42 -0700

    added FileRetention Utility

--- druid-lookups/src/main/java/com/yahoo/maha/maha_druid_lookups/server/lookup/namespace/FileRetention.java
+++ druid-lookups/src/main/java/com/yahoo/maha/maha_druid_lookups/server/lookup/namespace/FileRetention.java
@@ -1,5 +1,6 @@
 package com.yahoo.maha.maha_druid_lookups.server.lookup.namespace;
 
+import org.apache.commons.io.FileDeleteStrategy;
 import org.apache.commons.lang.time.DateUtils;
 import org.apache.druid.java.util.common.logger.Logger;
 import java.io.IOException;
@@ -9,13 +10,18 @@ import java.nio.file.Paths;
 import java.util.Date;
 import java.util.stream.Stream;
 
+/**
+ * FileRetention to be used to clean up files older than last modifed date.
+ * dir_path refers to the directory
+ * num_of_rentention_days refers to the number of date from sysdate the files should be retained.
+ * All files older than rentention date would be deleted
+ **/
 public class FileRetention {
     private static final Logger LOG = new Logger(FileRetention.class);
 
+    public void cleanup(String dir_path,int num_of_rentention_days){
 
-    public void cleanup(String dir_path,int num_of_rentention_days) throws IOException {
-
-        Date Retention_date = DateUtils.addDays(new Date(), num_of_rentention_days);
+        Date Retention_date = DateUtils.addDays(new Date(), num_of_rentention_days * -1);
         long Retention_date_epoch = Retention_date.getTime() / 1000L;
 
         try (Stream<Path> filePathStream = Files.walk(Paths.get(dir_path))) {
@@ -23,21 +29,21 @@ public class FileRetention {
                 if (Files.isRegularFile(filePath)) {
                     if(filePath.toFile().lastModified() <= Retention_date_epoch)
                     {
-                        filePath.toFile().delete();
-                       LOG.info(filePath.toFile().getName() + "deleted");
+                        try {
+                            FileDeleteStrategy.FORCE.delete(filePath.toFile());
+                            LOG.info("File " + filePath.toFile().getName() + "deleted at path " + filePath.toFile().getAbsolutePath());
+                        } catch (IOException e) {
+                            LOG.error("exception when trying to delete " + filePath.toFile().getName() + e);
+                        }
 
                     }
-
                 }
             });
-
         }
         catch (Exception e)
         {
-            LOG.info("Exception while trying to clean up "+ e);
+            LOG.error("Exception while trying to clean up "+ e);
         }
-
-
     }
 }
 
--- druid-lookups/src/test/java/com/yahoo/maha/maha_druid_lookups/query/lookup/FileRetentionTest.java
+++ druid-lookups/src/test/java/com/yahoo/maha/maha_druid_lookups/query/lookup/FileRetentionTest.java
@@ -2,17 +2,12 @@
 // Licensed under the terms of the Apache License 2.0. Please see LICENSE file in project root for terms.
 package com.yahoo.maha.maha_druid_lookups.query.lookup;
 
-
 import com.yahoo.maha.maha_druid_lookups.server.lookup.namespace.FileRetention;
 import org.apache.commons.lang.time.DateUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Date;
-
 
 public class FileRetentionTest {
 
@@ -20,40 +15,25 @@ public class FileRetentionTest {
     public void testCleanup() throws Exception {
         FileRetention FileRenention = new FileRetention();
         String[] pathnames;
-        Path tempDirWithPrefix = Files.createTempDirectory("prefix");
+        new File("target/rocksdbcleanup").mkdirs();
 
         Date Retention_date = DateUtils.addDays(new Date(), -12);
         long Retention_date_epoch = Retention_date.getTime() / 1000L;
 
-        File file1 = File.createTempFile( "Test", "1", new File(String.valueOf(tempDirWithPrefix)));
-        File file2 = File.createTempFile( "Test", "2", new File(String.valueOf(tempDirWithPrefix)));
-        File file3 = File.createTempFile( "Test", "3", new File(String.valueOf(tempDirWithPrefix)));
-        File file4 = File.createTempFile( "Test", "4", new File(String.valueOf(tempDirWithPrefix)));
-
+        File file1 = File.createTempFile( "Test", "1", new File("target/rocksdbcleanup"));
+        File file2 = File.createTempFile( "Test", "2", new File("target/rocksdbcleanup"));
+        File file3 = File.createTempFile( "Test", "3", new File("target/rocksdbcleanup"));
+        File file4 = File.createTempFile( "Test", "4", new File("target/rocksdbcleanup"));
 
         file1.setLastModified(Retention_date_epoch);
         file2.setLastModified(Retention_date_epoch);
 
-        FileRenention.cleanup(String.valueOf(tempDirWithPrefix),-10);
+        FileRenention.cleanup("target/rocksdbcleanup",10);
 
-        File f = new File(String.valueOf(tempDirWithPrefix));
+        File f = new File("target/rocksdbcleanup");
         pathnames = f.list();
         Assert.assertEquals(pathnames.length, 2);
         file3.deleteOnExit();
         file4.deleteOnExit();
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
