28090cf0dc064d92184ccd957825ef08acae7702
Nick Alterio <nick@alterio.org>
2021-07-20 20:20:26 -0400

    Update DefaultMahaCalciteSqlParserTest.scala

--- service/src/test/scala/com/yahoo/maha/service/calcite/DefaultMahaCalciteSqlParserTest.scala
+++ service/src/test/scala/com/yahoo/maha/service/calcite/DefaultMahaCalciteSqlParserTest.scala
@@ -393,4 +393,21 @@ class DefaultMahaCalciteSqlParserTest extends BaseMahaServiceTest with Matchers 
     assert(request.filterExpressions.head.asValues.equals("abc"))
     assert(request.filterExpressions.toString contains "NotEqualToFilter(Student Name,abc,false,false)")
   }
+  
+    test("test filter: less than") {
+
+    val sql =
+      s"""
+              select * from student_performance
+              where 'Student Name' < '123'
+              """
+
+    val mahaSqlNode: MahaSqlNode = defaultMahaCalciteSqlParser.parse(sql, StudentSchema, "er")
+    assert(mahaSqlNode.isInstanceOf[SelectSqlNode])
+    val request = mahaSqlNode.asInstanceOf[SelectSqlNode].reportingRequest
+    assert(request.requestType === SyncRequest)
+    assert(request.filterExpressions.size == 1)
+    assert(request.filterExpressions.head.field.equals("Student Name"))
+    assert(request.filterExpressions.toString contains "LessThanFilter(Student Name,123,false,false)")
+  }
 }
