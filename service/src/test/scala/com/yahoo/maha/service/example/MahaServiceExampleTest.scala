28d9bd7468e503a82c473de91aecbcb5bd324301
PRIYANKA DADLANI <pdadlani@verizonmedia.com>
2021-11-16 19:11:00 +0000

    Importing StandardCharsets

--- service/src/test/scala/com/yahoo/maha/service/example/MahaServiceExampleTest.scala
+++ service/src/test/scala/com/yahoo/maha/service/example/MahaServiceExampleTest.scala
@@ -1,6 +1,7 @@
 // Copyright 2017, Yahoo Holdings Inc.
 // Licensed under the terms of the Apache License 2.0. Please see LICENSE file in project root for terms.
 package com.yahoo.maha.service.example
+import java.nio.charset.StandardCharsets 
 
 import com.yahoo.maha.core.bucketing._
 import com.yahoo.maha.core.query.druid.{DruidQueryGenerator, SyncDruidQueryOptimizer}
