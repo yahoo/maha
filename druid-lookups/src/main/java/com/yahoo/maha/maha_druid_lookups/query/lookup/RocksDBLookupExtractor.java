4fa50b263a36e266bf30dcab9594f349b4bca5ee
Ritvik Jaiswal <ritvikjaiswal@users.noreply.github.com>
2022-01-13 12:11:26 -0800

    Fix iterables on Druid 21 lookups

--- druid-lookups/src/main/java/com/yahoo/maha/maha_druid_lookups/query/lookup/RocksDBLookupExtractor.java
+++ druid-lookups/src/main/java/com/yahoo/maha/maha_druid_lookups/query/lookup/RocksDBLookupExtractor.java
@@ -2,7 +2,9 @@
 // Licensed under the terms of the Apache License 2.0. Please see LICENSE file in project root for terms.
 package com.yahoo.maha.maha_druid_lookups.query.lookup;
 
+import com.yahoo.maha.maha_druid_lookups.server.lookup.namespace.RocksDBSnapshot;
 import com.yahoo.maha.maha_druid_lookups.server.lookup.namespace.schema.protobuf.ProtobufSchemaFactory;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.druid.java.util.common.logger.Logger;
 import org.apache.druid.java.util.emitter.service.ServiceEmitter;
 import com.yahoo.maha.maha_druid_lookups.query.lookup.namespace.RocksDBExtractionNamespace;
@@ -11,8 +13,16 @@ import com.yahoo.maha.maha_druid_lookups.server.lookup.namespace.LookupService;
 import com.yahoo.maha.maha_druid_lookups.server.lookup.namespace.RocksDBManager;
 import com.yahoo.maha.maha_druid_lookups.server.lookup.namespace.entity.CacheActionRunner;
 import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
+import org.rocksdb.RocksIterator;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentMap;
 
 public class RocksDBLookupExtractor<U> extends BaseRocksDBLookupExtractor<U> {
 
@@ -41,6 +51,46 @@ public class RocksDBLookupExtractor<U> extends BaseRocksDBLookupExtractor<U> {
 
     @Override
     public Iterable<Map.Entry<String, String>> iterable() {
-        return staticMap.entrySet();
+        if (rocksDBManager == null) {
+            LOG.info("rocksDBManager is null!!");
+        }
+        else {
+            LOG.info("rocksDBManager: " + rocksDBManager);
+        }
+        if (extractionNamespace == null) {
+            LOG.info("extractionNamespace is null!!");
+        }
+        else {
+            LOG.info("extractionNamespace: " + extractionNamespace);
+        }
+        if (extractionNamespace.getNamespace() == null) {
+            LOG.info("extractionNamespace.getNamespace() is null!!");
+        }
+        else {
+            LOG.info("extractionNamespace.getNamespace(): " + extractionNamespace.getNamespace());
+        }
+        final RocksDB db = rocksDBManager.getDB(extractionNamespace.getNamespace());
+        LOG.info("Got RocksDB db");
+        if (db == null) {
+            LOG.info("RocksDB db is null!!");
+        }
+        else {
+            LOG.info("RocksDB db: " + db.toString());
+        }
+
+        Map<String, String> tempMap = new java.util.HashMap<>();
+        RocksIterator it = db.newIterator();
+        LOG.info("Got RocksIterator it");
+        it.seekToFirst();
+        LOG.info("Got seekToFirst");
+        while (it.isValid()) {
+            LOG.info("Iterator is valid");
+            LOG.info("it.key: " + Arrays.toString(it.key()));
+            LOG.info("it.value: " + Arrays.toString(it.value()));
+            tempMap.put(Arrays.toString(it.key()), Arrays.toString(it.value()));
+            it.next();
+        }
+
+        return tempMap.entrySet();
     }
 }
