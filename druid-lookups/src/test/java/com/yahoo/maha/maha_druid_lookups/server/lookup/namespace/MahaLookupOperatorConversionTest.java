ee057fd44d5c77b082fdd22c38941886a8bccb79
Ryan Wagner <ryanwagner@oath.com>
2022-01-19 11:25:52 -0800

    Add Lookup JSON String output to test, make test util

--- druid-lookups/src/test/java/com/yahoo/maha/maha_druid_lookups/server/lookup/namespace/MahaLookupOperatorConversionTest.java
+++ druid-lookups/src/test/java/com/yahoo/maha/maha_druid_lookups/server/lookup/namespace/MahaLookupOperatorConversionTest.java
@@ -1,42 +1,42 @@
 package com.yahoo.maha.maha_druid_lookups.server.lookup.namespace;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import javafx.util.Pair;
 import org.apache.calcite.jdbc.JavaTypeFactoryImpl;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rex.RexBuilder;
 import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.sql.SqlOperator;
 import org.apache.calcite.sql.type.SqlTypeName;
 import org.apache.druid.common.config.NullHandling;
+import org.apache.druid.jackson.DefaultObjectMapper;
+import org.apache.druid.java.util.common.granularity.Granularities;
 import org.apache.druid.math.expr.ExprMacroTable;
+import org.apache.druid.query.groupby.GroupByQuery;
 import org.apache.druid.query.lookup.*;
 import org.apache.druid.segment.column.RowSignature;
 import org.apache.druid.segment.column.ValueType;
+import org.apache.druid.sql.calcite.aggregation.DimensionExpression;
 import org.apache.druid.sql.calcite.expression.DruidExpression;
 import org.apache.druid.sql.calcite.planner.PlannerContext;
 import org.apache.druid.sql.calcite.table.RowSignatures;
 
 import org.easymock.EasyMock;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 
+import java.util.Map;
+
 public class MahaLookupOperatorConversionTest {
-
-    private RexBuilder rexBuilder;
-    private RelDataTypeFactory typeFactory = new JavaTypeFactoryImpl();
-
-    private static final RowSignature ROW_SIGNATURE = RowSignature
-            .builder()
-            .add("d", ValueType.DOUBLE)
-            .add("l", ValueType.LONG)
-            .add("s", ValueType.STRING)
-            .add("student_id", ValueType.STRING)
-            .build();
+    MahaLookupTestUtil util = new MahaLookupTestUtil();
 
     @BeforeTest
     public void setUp(){
-        this.rexBuilder = new RexBuilder(typeFactory);
         NullHandling.initializeForTests();
     }
 
@@ -46,7 +46,16 @@ public class MahaLookupOperatorConversionTest {
     }
 
     @Test
-    public void testLookupReturnsExpectedResults() {
+    public void testLookupReturnsExpectedResults() throws JsonProcessingException {
+        RexBuilder rexBuilder = new RexBuilder(util.typeFactory);
+        RowSignature ROW_SIGNATURE = RowSignature
+                .builder()
+                .add("d", ValueType.DOUBLE)
+                .add("l", ValueType.LONG)
+                .add("s", ValueType.STRING)
+                .add("student_id", ValueType.STRING)
+                .build();
+
         final LookupExtractorFactoryContainerProvider manager = EasyMock.createStrictMock(LookupReferencesManager.class);
 
         MahaLookupOperatorConversion opConversion = new MahaLookupOperatorConversion(manager);
@@ -55,31 +64,23 @@ public class MahaLookupOperatorConversionTest {
         PlannerContext plannerContext = EasyMock.createStrictMock(PlannerContext.class);
         EasyMock.expect(plannerContext.getExprMacroTable()).andReturn(exprMacroTable).anyTimes();
         EasyMock.replay(plannerContext);
-        RexNode rn2 = rexBuilder.makeCall(opConversion.calciteOperator(),
-                makeInputRef("student_id"),
-                rexBuilder.makeLiteral(
-                        "student_lookup",
-                        typeFactory.createSqlType(SqlTypeName.VARCHAR), true
-                ),
-                rexBuilder.makeLiteral(
-                        "student_id",
-                        typeFactory.createSqlType(SqlTypeName.VARCHAR), true
-                ),
-                rexBuilder.makeLiteral(
-                        "123",
-                        typeFactory.createSqlType(SqlTypeName.VARCHAR), true
-                ));
+        MahaLookupTestUtil.MAHA_LOOKUP mahaLookup = new MahaLookupTestUtil.MAHA_LOOKUP(
+                util.makeInputRef("student_id", ROW_SIGNATURE, rexBuilder)
+                , new Pair<>("student_lookup", SqlTypeName.VARCHAR)
+                , new Pair<>("student_id", SqlTypeName.VARCHAR)
+                , new Pair<>("123", SqlTypeName.VARCHAR)
+                , rexBuilder
+        );
+
+        RexNode rn2 = mahaLookup.makeCall(rexBuilder, opConversion.calciteOperator());
 
         DruidExpression druidExp = opConversion.toDruidExpression(plannerContext, ROW_SIGNATURE, rn2);
         assert druidExp != null;
         String expectedDruidExpr = "DruidExpression{simpleExtraction=MahaRegisteredLookupExtractionFn{delegate=null, lookup='student_lookup', retainMissingValue=false, replaceMissingValueWith='123', injective=false, optimize=false, valueColumn=student_id, decodeConfig=null, useQueryLevelCache=false}(student_id), expression='maha'}";
+        String json = util.convertToJson(druidExp, "testing_stats", "Student ID");
         assert druidExp.toString().equals(expectedDruidExpr);
+        assert json.contains("\"dimensions\":[{\"type\":\"extraction\",\"dimension\":\"student_id\",\"outputName\":\"Student ID\",\"outputType\":\"STRING\",\"extractionFn\":{\"type\":\"mahaRegisteredLookup\",\"lookup\":\"student_lookup\",\"retainMissingValue\":false,\"replaceMissingValueWith\":\"123\",\"injective\":false,\"optimize\":false,\"valueColumn\":\"student_id\",\"decode\":null,\"dimensionOverrideMap\":null,\"useQueryLevelCache\":false}}]");
     }
 
-    RexNode makeInputRef(String columnName)
-    {
-        RelDataType relDataType = RowSignatures.toRelDataType(ROW_SIGNATURE, typeFactory);
-        int columnNumber = ROW_SIGNATURE.indexOf(columnName);
-        return rexBuilder.makeInputRef(relDataType.getFieldList().get(columnNumber).getType(), columnNumber);
-    }
+
 }
---
+++ druid-lookups/src/test/java/com/yahoo/maha/maha_druid_lookups/server/lookup/namespace/MahaLookupTestUtil.java
@@ -0,0 +1,81 @@
+package com.yahoo.maha.maha_druid_lookups.server.lookup.namespace;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import javafx.util.Pair;
+import org.apache.calcite.jdbc.JavaTypeFactoryImpl;
+import org.apache.calcite.rel.type.RelDataType;
+import org.apache.calcite.rel.type.RelDataTypeFactory;
+import org.apache.calcite.rex.RexBuilder;
+import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.sql.SqlOperator;
+import org.apache.calcite.sql.type.SqlTypeName;
+import org.apache.druid.jackson.DefaultObjectMapper;
+import org.apache.druid.java.util.common.granularity.Granularities;
+import org.apache.druid.query.groupby.GroupByQuery;
+import org.apache.druid.segment.column.RowSignature;
+import org.apache.druid.segment.column.ValueType;
+import org.apache.druid.sql.calcite.aggregation.DimensionExpression;
+import org.apache.druid.sql.calcite.expression.DruidExpression;
+import org.apache.druid.sql.calcite.table.RowSignatures;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+public class MahaLookupTestUtil {
+    RelDataTypeFactory typeFactory = new JavaTypeFactoryImpl();
+
+    DateTime todayTime = DateTime.now(DateTimeZone.UTC);
+    DateTime lastWeekTime = todayTime.minusDays(7);
+    String today = todayTime.toString("YYYY-MM-DD");
+    String lastWeek = lastWeekTime.toString("YYYY-MM-DD");
+
+    RexNode makeInputRef(String columnName, RowSignature ROW_SIGNATURE, RexBuilder rexBuilder)
+    {
+        RelDataType relDataType = RowSignatures.toRelDataType(ROW_SIGNATURE, typeFactory);
+        int columnNumber = ROW_SIGNATURE.indexOf(columnName);
+        return rexBuilder.makeInputRef(relDataType.getFieldList().get(columnNumber).getType(), columnNumber);
+    }
+
+    String convertToJson(DruidExpression druidExpression, String cubeName, String outputName) throws JsonProcessingException {
+        GroupByQuery query = GroupByQuery.builder()
+                .setDataSource(cubeName)
+                .setInterval(lastWeek + "T00:00:00.000Z/" + today + "T00:00:00.000Z")
+                .setGranularity(Granularities.ALL)
+                .setDimensions(
+                        DimensionExpression.ofSimpleColumn(outputName, druidExpression, ValueType.STRING).toDimensionSpec()
+                )
+                .build();
+
+        DefaultObjectMapper mapper = new DefaultObjectMapper();
+
+        return mapper.writeValueAsString(query);
+    }
+
+    static class MAHA_LOOKUP{
+        private RexNode inputRef;
+        private RexNode lookupName;
+        private RexNode lookupCol;
+        private RexNode replaceIfNull;
+        RelDataTypeFactory typeFactory = new JavaTypeFactoryImpl();
+
+        MAHA_LOOKUP(RexNode inputRef
+                , Pair<String, SqlTypeName> lookupName
+                , Pair<String, SqlTypeName> lookupCol
+                , Pair<String, SqlTypeName> replaceIfNull
+                , RexBuilder rexBuilder
+        ) {
+            this.inputRef = inputRef;
+            this.lookupName = makeLiteral(rexBuilder, lookupName.getKey(), lookupName.getValue(), true);
+            this.lookupCol = makeLiteral(rexBuilder, lookupCol.getKey(), lookupCol.getValue(), true);
+            this.replaceIfNull = makeLiteral(rexBuilder, replaceIfNull.getKey(), replaceIfNull.getValue(), true);
+        }
+
+        RexNode makeLiteral(RexBuilder rexBuilder, String literalName, SqlTypeName dataType, Boolean allowCast) {
+            return rexBuilder.makeLiteral(literalName, typeFactory.createSqlType(dataType), allowCast);
+        }
+
+        RexNode makeCall(RexBuilder rexBuilder, SqlOperator operator){
+            return rexBuilder.makeCall(operator, inputRef, lookupName, lookupCol, replaceIfNull);
+        }
+
+    }
+}
